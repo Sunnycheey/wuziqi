9945abd46022830f2012a2057a8d6908395a9e51
李辉超 <lihuichaolihc@gmail.com>
2019-10-12 00:45:40 +0800

    runnable demo with ncurses

--- board/display.cpp
+++ board/display.cpp
@@ -1,32 +1,83 @@
+// ncurses is used as UI in terminal
 #include "board.hpp"
 #include <iostream>
+#include <ncurses.h>
+#include "../logical/play.cpp"
+
 using std::cout; using std::endl; using std::vector;
 void board::display(){
     // this function is used to display the game board
     // we use * to represent the blank position of board, + represent the pieces of player1 and - represent the pieces of player2.
     vector<point> p1;
     vector<point> p2;
+    player user = player1;
     for(auto it = v.begin(); it != v.end(); ++it){
        if(it->p == player1) p1.push_back(it->c);
        if(it->p == player2) p2.push_back(it->c);
     }
-    // using a two dimensional char array to represent the board
-    char game_board[ROW][COLUMN];
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+    // make an original board
+    // int row, column;
+    // getmaxyx(stdscr, row, column);
     for(int i = 0; i < ROW; i++){
         for(int j = 0; j < COLUMN; j++){
-            game_board[i][j]='*';
+            mvaddch(i, j, '*');
         }
     }
-    for(auto it = p1.begin(); it != p1.end(); ++it){
-        game_board[it->x][it->y] = '+';
-    }
-    for(auto it = p2.begin(); it != p2.end(); ++it){
-        game_board[it->x][it->y] = '-';
-    }
-    for(int i = 0; i < ROW; i++){
-        for(int j = 0; j < COLUMN; j++){
-            cout << game_board[i][j] << " ";
+    // set the position of cursor to (0, 0)
+    move(0,0);
+    refresh();
+    while(true){
+        int ch = getch();
+        point p(0,0);
+        switch(ch){
+            case KEY_LEFT:
+                // move the window to left by one unit
+               getyx(stdscr, p.x, p.y);
+               if(p.y != 0) move(p.x, p.y-1);
+               refresh();
+               // else do nothig
+                break;
+            case KEY_RIGHT:
+                getyx(stdscr, p.x, p.y);
+                if(p.y != COLUMN-1) move(p.x, p.y+1);
+                refresh();
+                break;
+            case KEY_UP:
+                getyx(stdscr, p.x, p.y);
+                if(p.x != 0) move(p.x-1, p.y);
+                refresh();
+                break;
+            case KEY_DOWN:
+                getyx(stdscr, p.x, p.y);
+                if(p.x != ROW-1) move(p.x+1, p.y);
+                refresh();
+                break;
+            case 10:
+                // case 10 represent enter
+                getyx(stdscr, p.x, p.y);
+                if(user == player1) {
+                    addch('+');
+                }
+                else if(user == player2) {
+                    addch('-');
+                }
+                if(!play(this, &p, user)){
+                    return ;
+                    endwin();
+                }
+                terminfo t = this->isterminated();
+                if(t.win == true){
+                    endwin();
+                    return;
+                }
+                refresh();
+                user = (user==player1)?player2:player1;
+                break;
         }
-        cout << endl;
     }
+    endwin();
 }
--- main.cpp
+++ main.cpp
@@ -1,14 +1,11 @@
 #include "./board/board.hpp"
 #include "./board/display.cpp"
 #include "./logical/judge.cpp"
-#include "./logical/play.cpp"
 
 using std::cin; 
 int main(){
     // make a board
     board b;
-    player p = player1;
-    // set inital row and column
     cout << "please indicate the row and column of board" << endl;
     cin >> ROW >> COLUMN;
     if(ROW < 0 || COLUMN < 0) {
@@ -16,27 +13,13 @@ int main(){
         return -1;
     }
     while(true){
-        cout << "\033[2J\033[1;1H";"]]";
-        b.display(); 
         int x, y;
-        cout << "player" << p + 1<< " pieces location: (in x, y format)" << endl;
-        cin >> x >> y;
         // verificate whether x and y are valid
         if(x >= ROW || x < 0 || y > COLUMN || y < 0) {
             cout << "Pieces cannot given the position out of board!" << endl;
             return -1;
         }
-        point c(x,y);
-        if(!play(&b, &c, p)) {
-            cout << "The position is already used!" << endl;
-            return -1;
-        }
-        terminfo t = b.isterminated();
-        if(t.win == true){
-            cout << "player" << t.p+1 << "win!" << endl; 
-            return 0;
-        } 
-        p = (p==player1)?player2:player1;
-    }
+        b.display();
     return -1;
+    }
 }
